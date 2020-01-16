a85651643b4fdf35cf7accfa3da5c5e20a425a42
Dylan <dpaiton@gmail.com>
2020-01-16 13:45:55 +0100

    adds eps default param

--- params/base_params.py
+++ params/base_params.py
@@ -13,6 +13,7 @@ class BaseParams(object):
         self.model_type = None
         self.log_to_file = True
         self.dtype = torch.float
+        self.eps = 1e-12
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.rand_seed = 123456789
         self.rand_state = np.random.RandomState(self.rand_seed)
